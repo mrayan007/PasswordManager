cae6fc73c71e452f77a921d17c45ebc00745a30d
mrayan007 <rayanwmv@gmail.com>
2025-07-19 22:09:41 +0200

    prog

---
+++ password-manager.py
@@ -0,0 +1,6 @@
+# questionary is a library that allows me to create a cli with highlighted and selectable choices
+import questionary
+
+menu = [
+    ""
+]
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Python Password Manager
+
+## Checklist
+
+* 
